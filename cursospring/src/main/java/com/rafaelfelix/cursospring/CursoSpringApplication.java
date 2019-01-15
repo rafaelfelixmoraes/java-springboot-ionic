21e950655edbcc6b9430e861c3b0a676730901af
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-01-15 18:12:16 -0200

    Projeto criado

---
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/CursoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.rafaelfelix.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+}
+
