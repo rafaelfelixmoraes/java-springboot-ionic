94a70ba604d7d90604d8b2cae8d1e700601d6b08
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-01-20 12:53:31 -0200

    Testando a primeria classe de domínio

---
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/domain/Categoria.java
@@ -0,0 +1,71 @@
+package com.rafaelfelix.cursospring.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1992511773778559354L;
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+}
--- cursospring/src/main/java/com/rafaelfelix/cursospring/resources/CategoriaResource.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.rafaelfelix.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelfelix.cursospring.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping("/listar")
-	public String listar() {
-		return new String("Olha , parece que o Rest está funcionando hein...");
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática"); 
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>(); 
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
