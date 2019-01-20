29e4d6cf7eed608900142d8798d7de9f2be309cd
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-01-20 13:20:40 -0200

    Banco de Dados H2 e criação da base de dados automaticamente

--- cursospring/src/main/java/com/rafaelfelix/cursospring/domain/Categoria.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/domain/Categoria.java
@@ -3,12 +3,17 @@ package com.rafaelfelix.cursospring.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	/**
-	 * 
-	 */
+@Entity
+public class Categoria implements Serializable{
 	private static final long serialVersionUID = -1992511773778559354L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
