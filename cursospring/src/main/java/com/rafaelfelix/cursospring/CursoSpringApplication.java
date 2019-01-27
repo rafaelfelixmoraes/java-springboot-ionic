254ff23d286fa4c0c02dcf82dd123d8faa3efe8b
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-01-27 13:51:37 -0200

    Estado e Cidade

--- cursospring/src/main/java/com/rafaelfelix/cursospring/CursoSpringApplication.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/CursoSpringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafaelfelix.cursospring.domain.Categoria;
+import com.rafaelfelix.cursospring.domain.Cidade;
+import com.rafaelfelix.cursospring.domain.Estado;
 import com.rafaelfelix.cursospring.domain.Produto;
 import com.rafaelfelix.cursospring.repositories.CategoriaRepository;
+import com.rafaelfelix.cursospring.repositories.CidadeRepository;
+import com.rafaelfelix.cursospring.repositories.EstadoRepository;
 import com.rafaelfelix.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoSpringApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -34,6 +44,13 @@ public class CursoSpringApplication implements CommandLineRunner{
 		Produto prod2 = new Produto(null, "Impressora", 800.00);
 		Produto prod3 = new Produto(null, "Mouse", 80.00);
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
 		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
 		cat2.getProdutos().addAll(Arrays.asList(prod2));
 		
@@ -41,8 +58,13 @@ public class CursoSpringApplication implements CommandLineRunner{
 		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		prod3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 	}
 
 }
---
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/domain/Estado.java
@@ -0,0 +1,100 @@
+package com.rafaelfelix.cursospring.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Estado implements Serializable{
+	private static final long serialVersionUID = -5680411397809660450L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
+	
+	public Estado () {
+		
+	}
+
+	public Estado(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the cidades
+	 */
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	/**
+	 * @param cidades the cidades to set
+	 */
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
