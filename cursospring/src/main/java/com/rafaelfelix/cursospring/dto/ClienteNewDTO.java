ae22049fb614eef0d61e04108cc029e5a939f53d
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-02-24 14:54:31 -0300

    Validação customizada: CPF ou CNPJ na inserção de Cliente

--- cursospring/src/main/java/com/rafaelfelix/cursospring/dto/ClienteNewDTO.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/dto/ClienteNewDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.rafaelfelix.cursospring.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
