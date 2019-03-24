a9b45ae96609bd1952645df64b4b7b3ac3e3bf75
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-03-24 15:47:07 -0300

    - Retirando import desnecessário

--- cursospring/src/main/java/com/rafaelfelix/cursospring/resources/PedidoResource.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/resources/PedidoResource.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.rafaelfelix.cursospring.domain.Categoria;
 import com.rafaelfelix.cursospring.domain.Pedido;
-import com.rafaelfelix.cursospring.dto.CategoriaDTO;
 import com.rafaelfelix.cursospring.services.PedidoService;
 
 @RestController
