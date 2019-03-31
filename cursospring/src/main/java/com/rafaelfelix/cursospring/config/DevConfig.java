bec3663e5f14ff1d1872ca8280311661ec86e7f3
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-03-31 15:49:59 -0300

    Criando profile de desenvolvimento

---
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.rafaelfelix.cursospring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rafaelfelix.cursospring.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		
+		if(strategy.equalsIgnoreCase("create")) {
+			return false;
+		}
+		return true;
+	}
+
+}
