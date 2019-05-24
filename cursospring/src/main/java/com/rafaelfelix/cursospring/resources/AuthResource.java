df81ff0eb8927d304899daadb94c8be02b9f431e
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-05-24 13:52:37 -0300

    Expondo o header Authorization (problema de Cors)

--- cursospring/src/main/java/com/rafaelfelix/cursospring/resources/AuthResource.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/resources/AuthResource.java
@@ -32,6 +32,7 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		
 		return ResponseEntity.noContent().build();
 	}
