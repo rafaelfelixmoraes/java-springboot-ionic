3e8b2c73fbea39eb75860016fb445cf14698afbe
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-05-27 12:00:49 -0300

    - Ajustes nas classes que não tinham @Service

--- cursospring/src/main/java/com/rafaelfelix/cursospring/services/UserService.java
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/services/UserService.java
@@ -1,9 +1,11 @@
 package com.rafaelfelix.cursospring.services;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import com.rafaelfelix.cursospring.security.UserSS;
 
+@Service
 public class UserService {
 
 	public static UserSS authenticated() {
