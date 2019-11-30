e0b9fa6296d7127d1a63c1429da97842717f75b7
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2019-11-30 19:12:08 -0300

    Últimos ajustes para deploy em prod

---
+++ cursospring/src/main/java/com/rafaelfelix/cursospring/config/ProdConfig.java
@@ -0,0 +1,41 @@
+package com.rafaelfelix.cursospring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rafaelfelix.cursospring.services.DBService;
+import com.rafaelfelix.cursospring.services.EmailService;
+import com.rafaelfelix.cursospring.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!strategy.equalsIgnoreCase("create")) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+
+}
